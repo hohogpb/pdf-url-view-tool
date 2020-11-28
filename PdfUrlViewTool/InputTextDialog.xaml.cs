6d47be2db033198e8aa83d103102960c2dbe8085
gpb <512167281@qq.com>
2020-11-28 17:07:48 +0800

    init commit 记得copy libmupdf到bin下

---
+++ PdfUrlViewTool/InputTextDialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PdfUrlViewTool
+{
+    /// <summary>
+    /// InputTextDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class InputTextDialog : Window
+    {
+        public InputTextDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static DependencyProperty TxtBoxValueProperty = DependencyProperty.Register(
+            "TxtBoxValue", typeof(String), typeof(InputTextDialog));
+
+        public String TxtBoxValue
+        {
+            get { return (String)GetValue(TxtBoxValueProperty); }
+            set
+            {
+                SetValue(TxtBoxValueProperty, value);
+            }
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            if (e.Property == TxtBoxValueProperty)
+            {
+                // Do whatever you want with it
+            }
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
---
+++ PdfUrlViewTool/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using GalaSoft.MvvmLight;
+using MoonPdfLib.MuPdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PdfUrlViewTool
+{
+    public class PdfEntry : ViewModelBase
+    {
+        private string pdfUrl;
+        public string PdfUrl
+        {
+            get { return pdfUrl; }
+            set { Set(ref pdfUrl, value); }
+        }
+    }
+
+
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+            PdfEntries = new BindingList<PdfEntry>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private List<string> pdfList;
+        public List<string> PdfList
+        {
+            get { return pdfList; }
+            set
+            {
+                pdfList = value;
+                RaisePropertyChanged("PdfList");
+            }
+        }
+
+        public BindingList<PdfEntry> pdfEntries;
+        public BindingList<PdfEntry> PdfEntries
+        {
+            get { return pdfEntries; }
+            set
+            {
+                pdfEntries = value;
+                pdfEntries.ListChanged += PdfEntries_ListChanged; ;
+                RaisePropertyChanged("PdfEntries");
+            }
+        }
+
+        private void PdfEntries_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Uri uri = new Uri(e.AddedItems[0].ToString());
+
+                using (var client = new WebClient())
+                {
+                    var pdf_data = client.DownloadData(uri);
+                    if (pdf_data.Length <= 0)
+                        throw new Exception(string.Format("pdf文件:{0} 有问题，未下载成功", uri));
+
+                    moonPdfPanel.Open(new MemorySource(pdf_data));
+                    //moonPdfPanel.ZoomToHeight();
+                    moonPdfPanel.ZoomToWidth();
+                    
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            InputTextDialog dlg = new InputTextDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                string txt = dlg.TxtBoxValue;
+                string[] lines = txt.Split('\n');
+                PdfList = lines.ToList();
+            }
+        }
+    }
+}
